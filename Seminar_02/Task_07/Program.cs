cac659fb96a43055bacc585a64c8e0c0024dd648
SMV <kraft088@gmail.com>
2023-03-02 23:18:52 +1100

    Ad  Task07

---
+++ Seminar_02/Task_07/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вычислить сумму от 2 ^ 0 до 2 ^ 10 (^ - степень).
+
+int n = 2;
+int sum = 0;
+
+for (int i = 0; i <= 10; i++)
+{
+    sum += (int)Math.Pow(n, i);
+}
+System.Console.WriteLine(sum);
