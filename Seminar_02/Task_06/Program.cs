979291445d9a37d6293a9af000f88fa00fac8729
SMV <kraft088@gmail.com>
2023-03-02 23:01:28 +1100

    Ad  Task06

---
+++ Seminar_02/Task_06/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано натуральное n. вычислить: 1 + 1/2 + 1/3 + 1/4 + ... + 1/n.
+
+System.Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+float sum = 0; // Число с плавающей точкой одинарной точности, можно еще 
+//использовать double.
+for (int i = 1; i <= n; i++)
+{
+    sum += (float)1 / (float)i; //(float) указывается для перевода типа данных 
+    // из int в float.
+    System.Console.WriteLine((float)1 / (float)i);
+}
+System.Console.WriteLine(sum);
