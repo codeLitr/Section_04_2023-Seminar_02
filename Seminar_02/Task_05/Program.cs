a72df4ddd1ed076e91e24a5e696197ea66fb1bbb
SMV <kraft088@gmail.com>
2023-03-02 22:43:49 +1100

    Ad  Task05

---
+++ Seminar_02/Task_05/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, 
+// является ли одно число квадратом другого.
+
+System.Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+//if(Math.Pow(firstNumber, 2) == secondNumber || Math.Pow(secondNumber, 2) == firstNumber)
+// Данную строку можно сократить через булевую переменную (bool). 
+// Когда вводится булевая перменная, то перед названием переменной ставится "is".
+bool isSquare = Math.Pow(firstNumber, 2) == secondNumber || Math.Pow(secondNumber, 2) == firstNumber;
+if(isSquare)
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
